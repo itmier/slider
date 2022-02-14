28d3abc8dddbc60c1e54350815912bb89d22f1d3
王云飞 <itmier@126.com>
2022-02-14 17:11:05 +0800

    '轮播图0.6'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 王云飞
  * @Date: 2022-01-27 15:27:53
- * @LastEditTime: 2022-02-14 15:10:30
+ * @LastEditTime: 2022-02-14 15:38:59
  * @LastEditors: 王云飞
  * @Description: 
  * 
@@ -15,50 +15,9 @@ class Slider {
     .querySelectorAll('.slider-list__item, .slider-list__item--selected');
     // 初始化自动下一个时间
     this.cycle = cycle
-    // 获取控制器盒子
-    const controller = this.container.querySelector('.slide-list__control')
-    if(controller) {
-      // 获取所有控制器
-      const buttons = controller.querySelectorAll('.slide-list__control-buttons, .slide-list__control-buttons--selected');
-      // 添加 mouseover 事件, 当鼠标悬停在某个子控制器时, 跳转到和子控制器相同index的轮播图, 并停止自动轮播
-      controller.addEventListener('mouseover', evt=>{
-        const idx = Array.from(buttons).indexOf(evt.target);
-        if(idx >= 0){
-          this.slideTo(idx);
-          this.stop();
-        }
-      });
-      // 当鼠标离开子控制器时, 开始自动轮播
-      controller.addEventListener('mouseout', evt=>{
-        this.start();
-      });
-      // 添加自定义事件 slide 
-      this.container.addEventListener('slide', evt => {
-        const idx = evt.detail.index
-        const selected = controller.querySelector('.slide-list__control-buttons--selected');
-        if(selected) selected.className = 'slide-list__control-buttons';
-        buttons[idx].className = 'slide-list__control-buttons--selected';
-      })
-    }
-    const previous = this.container.querySelector('.slide-list__previous');
-    if(previous){
-      previous.addEventListener('click', evt => {
-        this.stop();
-        this.slidePrevious();
-        this.start();
-        evt.preventDefault();
-      });
-    }
-    
-    const next = this.container.querySelector('.slide-list__next');
-    if(next){
-      next.addEventListener('click', evt => {
-        this.stop();
-        this.slideNext();
-        this.start();
-        evt.preventDefault();
-      });
-    }
+  }
+  registerPlugins(...plugins) {
+    plugins.forEach(plugin => plugin(this))
   }
   // 获取当前展示的元素DOM
   getSelectedItem(){
@@ -97,6 +56,9 @@ class Slider {
       % this.items.length;
     this.slideTo(previousIdx);  
   }
+  addEventListener(type, handler) {
+    this.container.addEventListener(type, handler)
+  }
   start() {
     this.stop()
     this._timer = setInterval(() => this.slideNext(), this.cycle)
@@ -105,6 +67,56 @@ class Slider {
     clearInterval(this._timer)
   }
 }
+function pluginController(slider) {
+  // 获取控制器盒子
+  const controller = slider.container.querySelector('.slide-list__control');
+  if(controller){
+    // 获取控制圆点DOM
+    const buttons = controller.querySelectorAll('.slide-list__control-buttons, .slide-list__control-buttons--selected');
+    // 添加鼠标悬停监听, 悬停到哪个圆点, 跳转到哪个圆点所对应的轮播图, 并停止自动轮播
+    controller.addEventListener('mouseover', evt=>{
+      const idx = Array.from(buttons).indexOf(evt.target);
+      if(idx >= 0){
+        slider.slideTo(idx);
+        slider.stop();
+      }
+    });
+    // 添加鼠标离开监听, 离开后开始自动轮播
+    controller.addEventListener('mouseout', evt=>{
+      slider.start();
+    });
+    // 添加自定义事件 slide , 使控制器圆点与轮播图同步
+    slider.addEventListener('slide', evt => {
+      const idx = evt.detail.index
+      const selected = controller.querySelector('.slide-list__control-buttons--selected');
+      if(selected) selected.className = 'slide-list__control-buttons';
+      buttons[idx].className = 'slide-list__control-buttons--selected';
+    });
+  }  
+}
+function pluginPrevious(slider) {
+  const previous = slider.container.querySelector('.slide-list__previous');
+  if(previous){
+    previous.addEventListener('click', evt => {
+      slider.stop();
+      slider.slidePrevious();
+      slider.start();
+      evt.preventDefault();
+    });
+  } 
+}
+function pluginNext(slider) {
+  const next = slider.container.querySelector('.slide-list__next');
+  if(next){
+    next.addEventListener('click', evt => {
+      slider.stop();
+      slider.slideNext();
+      slider.start();
+      evt.preventDefault();
+    });
+  }  
+}
 
 const slider = new Slider('my-slider');
+slider.registerPlugins(pluginController, pluginPrevious, pluginNext)
 slider.start();
